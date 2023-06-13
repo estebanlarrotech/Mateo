897728d44ef695129efd94bd8ccc63ad1a209c55
estebanlarrotech <esteban.larrotech@gmail.com>
2023-06-13 19:25:31 +0100

    text adjusted

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ togglebtn.addEventListener("click", function(){
 })
 
 var typed=new Typed(".input",{
-    strings:["cores do Porto","cores do Benfica","cores do tua equipa","cores que voce goste",
-    "cores do Porto","cores do Benfica","cores do tua equipa","cores que voce goste",
-    "cores do Porto","cores do Benfica","cores do tua equipa","cores que voce goste"],
+    strings:["  cores do Porto","  cores do Benfica","  cores do tua equipa"," cores que você goste",
+    "  cores do Porto","  cores do Benfica","  cores do tua equipa","  cores que você goste",
+    "  cores do Porto","  cores do Benfica","  cores do tua equipa","  cores que você goste"],
     typedSpeed:20,
     backSpeed:25,
     loop:true
 })
 
 var typed=new Typed(".input2",{
-    strings:["  Anio = 0.50  ","","   Pulseira = 1  ","  Coliar = 2  "],
+    strings:["    ","","   Pulseira = 2€  ","  Pulseira = 1€   "],
     typedSpeed:20,
     backSpeed:25,
     loop:true
